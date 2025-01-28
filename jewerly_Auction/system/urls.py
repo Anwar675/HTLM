489df5952264e3d5dfa4d8065830369f1f6152e2
lqphu2005 <lqphu369@gmail.com>
2025-01-28 20:18:05 +0700

    Update urls.py

--- jewerly_Auction/system/urls.py
+++ jewerly_Auction/system/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('valuation/',views.diamond_valuation_view, name='valuation'),
-    path('print/', views.print_valuation_view, name='print_valuation'),
+    path('print_valuation/', views.print_valuation_view, name='print_valuation'),
 ]
